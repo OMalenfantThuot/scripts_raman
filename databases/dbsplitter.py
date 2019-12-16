f9706f43520f418d8a686ff7f2fed1d3f3a37e82
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2019-12-16 10:16:46 -0500

    shuffle indexes in splitter

--- databases/dbsplitter.py
+++ databases/dbsplitter.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+import numpy as np
 from ase.db import connect
 
 
@@ -15,12 +16,14 @@ class DbSplitter:
             self.dbname.strip(".db") + "_2.db",
         )
         with connect(self.dbname) as db, connect(out1) as db1, connect(out2) as db2:
-            idx1, idx2 = range(1, self.split + 1), range(self.split + 1, db.count() + 1)
+            idx = np.arange(1, db.count() + 1)
+            np.random.shuffle(idx)
+            idx1, idx2 = idx[:self.split], idx[self.split:]
             for idx in idx1:
-                row = db.get(id=idx)
+                row = db.get(id=idx.item())
                 db1.write(row)
             for idx in idx2:
-                row = db.get(id=idx)
+                row = db.get(id=idx.item())
                 db2.write(row)
 
 
