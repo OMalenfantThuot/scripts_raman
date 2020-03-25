f95e1e03b13151e4093c758659c2aceb95c28415
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2020-03-25 14:49:03 -0400

    graphene data - 0,1,2 defects implemented

--- databases/graphene_data/graphene_data.py
+++ databases/graphene_data/graphene_data.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 
-from mybigdft import Posinp, InputParams, Job, Logfile
+from mybigdft import Posinp, InputParams, Job, Logfile, Atom
 from shutil import rmtree, copyfile
 from copy import deepcopy
 import numpy as np
 import argparse
+import sys
 import os
 
 sys.path.append("/lustre03/project/6004866/olimt/raman/scripts_raman/")
@@ -30,16 +31,52 @@ def main(args):
     os.chdir("run_dir/")
 
     if args.n_defects == 0:
+        
         for i in range(1, args.n_structs + 1):
             posinp = generate_random_structure(initpos)
             run(posinp, i, args, param, pseudos)
+
     elif args.n_defects == 1:
-        pass
+        
+        initpos, _ = place_first_nitrogen(initpos)
+        
+        for i in range(1, args.n_structs + 1):
+            posinp = generate_random_structure(initpos)
+            run(posinp, i, args, param, pseudos)
+
     elif args.n_defects == 2:
-        pass
+
+        initpos, first_idx = place_first_nitrogen(initpos)
+
+        root = np.sqrt(args.n_structs)
+        if root % 1 == 0:
+            n_angle, n_radius = root, root
+        else:
+            n_angle, n_radius = np.ceil(root), np.floor(root)
+        max_radius = np.max(np.array(initpos.cell) / 2)
+        radiuses = np.linspace(1.0, max_radius, n_radius)
+        angles = np.linspace(0, 2 * np.pi, n_angle)
+
+        i = 0
+        #distances = np.zeros(int(n_angle * n_radius))
+        for theta in angles:
+            for r in radiuses:
+                i += 1 
+                posinp, second_idx = place_second_nitrogen(initpos, theta, r, first_idx)
+        #        distances[i-1] = np.linalg.norm(posinp.positions[first_idx] - posinp.positions[second_idx])
+                run(posinp, i, args, param, pseudos)
+        #np.savetxt("distances.data", distances)
+
     elif args.n_defects == 3:
-        pass
-    elif raise NotImplementedError("No method for this number of defects.")
+        root = np.sqrt(args.n_structs)
+        if root % 1 == 0:
+            n_angle, n_radius = root, root
+        else:
+            n_angle, n_radius = np.ceil(root), np.floor(root)
+
+    else:
+        raise NotImplementedError("No method for this number of defects.")
+
 
 def run(posinp, i, args, param, pseudos):
     try:
@@ -74,22 +111,41 @@ def generate_random_structure(initpos):
     n_translations = np.random.randint(len(pos)) + 1
     trans_idx = np.random.choice(atoms_idx, n_translations)
     for j in trans_idx:
-        phi = np.random.rand(0, 2 * np.pi)
-        theta = np.arccos(np.random.rand(-1.0, 1.0))
-        r = radius * np.cubrt(np.random.rand(0.0, 1.0))
+        phi = 2 * np.pi * np.random.rand()
+        theta = np.arccos(2 * np.random.rand() - 1)
+        r = radius * np.cbrt(np.random.rand())
         pos = pos.translate_atom(
             j,
             [
-                r * np.sin(theta) * cos(phi),
+                r * np.sin(theta) * np.cos(phi),
                 r * np.sin(theta) * np.sin(phi),
                 r * np.cos(theta),
             ],
         )
     return pos
 
+def place_first_nitrogen(posinp):
+    distances_to_middle = np.linalg.norm(
+        posinp.positions - np.array(posinp.cell) / 2, axis=1
+    )
+    idx = np.argmin(distances_to_middle)
+    posinp.atoms[idx] = Atom("N", posinp.atoms[idx].position)
+    return posinp, idx
 
-def create_parser(self):
-    parser = argparse.ArgumentParser(add_help=False)
+def place_second_nitrogen(initpos, theta, r, first_idx):
+    posinp = deepcopy(initpos)
+    point = np.array(posinp.cell) / 2 + np.array([r * np.cos(theta), 0, r * np.sin(theta)])
+    distances_to_point = np.linalg.norm(posinp.positions - point, axis=1)
+    weigths = np.exp(-distances_to_point ** 2 / 2)
+    weigths[first_idx] = 0
+    weigths = weigths/np.sum(weigths)
+    second_idx = int(np.random.choice(len(posinp), 1, p=weigths))
+    posinp.atoms[second_idx] = Atom("N", posinp.atoms[second_idx].position)
+    return posinp, second_idx
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         "name", type=str, help="Name to give to calculations.", default="Jobname"
     )
