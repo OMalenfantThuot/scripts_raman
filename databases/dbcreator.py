2a96f5e11d4c0905118fa6979ca2d296f0091a51
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2020-11-19 16:34:30 -0500

    database creation with abinit data

--- databases/dbcreator.py
+++ databases/dbcreator.py
@@ -6,23 +6,31 @@ import sys
 import argparse
 import numpy as np
 from ase.db import connect
+from ase.io import read
 from mlcalcdriver import Posinp
 from mlcalcdriver.interfaces import posinp_to_ase_atoms
+from abipy.abio.outputs import AbinitOutputFile
 from utils.global_variables import DEFAULT_METADATA
 
 
-class DbCreator:
-    def __init__(self):
-        self.parser = self._create_parser()
-        args = self.parser.parse_args()
-        self.dbname = args.dbname
+def create_parser():
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("dbname", help="Name of the database to create.")
+    parser.add_argument(
+        "run_mode",
+        choices=["abinit", "bigdft"],
+        help="Run mode used when generating the data.",
+    )
+    return parser
 
-    def create(self):
-        files = [f for f in os.listdir() if f.endswith(".xyz")]
-        with connect(self.dbname) as db:
-            db.metadata = DEFAULT_METADATA
+
+def main(args):
+    with connect(args.dbname) as db:
+        db.metadata = DEFAULT_METADATA
+        if args.run_mode == "bigdft":
+            files = [f for f in os.listdir() if f.endswith(".xyz")]
             for f in files:
-                posinp = Posinp.from_file(f)
+                atoms = posinp_to_ase_atoms(Posinp.from_file(f))
                 with open(f, "r") as posinp_file:
                     energy = float(posinp_file.readline().split()[2]) * 27.21138602
                     forces = None
@@ -31,36 +39,34 @@ class DbCreator:
                             continue
                         else:
                             forces = []
-                            for _ in range(len(posinp)):
+                            for _ in range(len(atoms)):
                                 forces.append(
                                     [
-                                        float(force) * 27.21138602 / 0.529177249
+                                        float(force)
                                         for force in posinp_file.readline().split()[-3:]
                                     ]
                                 )
-                            forces = np.array(forces)
+                            forces = np.array(forces) * 27.21138602 / 0.529177249
                             break
-                atoms = posinp_to_ase_atoms(posinp)
                 db.write(atoms, data={"energy": energy, "forces": forces})
 
-    def _create_parser(self):
-        parser = argparse.ArgumentParser(add_help=True)
-        parser.add_argument("dbname", help="Name of the database to create.")
-        return parser
-
-    @property
-    def dbname(self):
-        return self._dbname
-
-    @dbname.setter
-    def dbname(self, dbname):
-        dbname = str(dbname)
-        if dbname.endswith(".db"):
-            self._dbname = dbname
-        else:
-            self._dbname = dbname + ".db"
+        elif args.run_mode == "abinit":
+            files = [f for f in os.listdir() if f.endswith(".out")]
+            for f in files:
+                atoms = read(f, format="abinit-out")
+                about = AbinitOutputFile(f)
+                energy = float(about.final_vars_global["etotal"]) * 27.21138602
+                forces = (
+                    np.array(
+                        [float(g) for g in about.final_vars_global["fcart"].split()]
+                    ).reshape(-1, 3)
+                    * 27.21138602
+                    / 0.529177249
+                )
+                db.write(atoms, data={"energy": energy, "forces": forces})
 
 
 if __name__ == "__main__":
-    dbc = DbCreator()
-    dbc.create()
+    parser = create_parser()
+    args = parser.parse_args()
+    main(args)
