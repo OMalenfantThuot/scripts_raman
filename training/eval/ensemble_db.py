d94f36f6d45fe45de04ba1c4c8a7b3340eaec4be
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2021-07-26 16:42:31 -0400

    first version ensemble calculators evaluator

---
+++ training/eval/ensemble_db.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import os
+import csv
+import torch
+import argparse
+import numpy as np
+import mlcalcdriver
+from ase.db import connect
+from mlcalcdriver import Posinp, Job
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dbpath", help="Path to the database to evaluate.")
+    parser.add_argument("properties", help="Properties to evaluate.", nargs="*")
+    parser.add_argument("--models", help="Paths to the trained models.", default=[], nargs="*")
+    parser.add_argument("--name", help="Name of the evaluation file.", default=None)
+    parser.add_argument(
+        "--overwrite", help="Remove previous evaluation file", action="store_true"
+    )
+    parser.add_argument("--cuda", help="Use GPU for evaluation", action="store_true")
+    return parser
+
+
+def main(args):
+
+    eval_name = args.models[0].split("/")[-1] if args.name is None else args.name
+    eval_file = "evaluation_" + eval_name + ".txt"
+    if os.path.exists(eval_file):
+        if args.overwrite:
+            os.remove(eval_file)
+        else:
+            raise Exception(
+                "The evaluation file already exists. Delete it or add the overwrite flag."
+            )
+    device = "cuda" if args.cuda else "cpu"
+
+    calculator = mlcalcdriver.calculators.EnsembleCalculator(
+        args.models, device=device
+    )
+
+    with connect(args.dbpath) as db:
+        answers, results, stds = (
+            [[] for _ in range(len(args.properties))],
+            [[] for _ in range(len(args.properties))],
+            [[] for _ in range(len(args.properties))],
+        )
+        for row in db.select():
+            for i, prop in enumerate(args.properties):
+                answers[i].append(row.data[prop])
+            posinp = Posinp.from_ase(row.toatoms())
+
+            job = Job(posinp=posinp, calculator=calculator)
+            for i, prop in enumerate(args.properties):
+                job.run(prop)
+                results[i].append(job.results[prop].tolist())
+                stds[i].append(job.results[prop + "_std"].tolist())
+
+    header, error = [], []
+    for prop in args.properties:
+        header += ["MAE_" + prop, "%Error_" + prop, "STD_" + prop]
+    for l1, l2, l3 in zip(answers, results, stds):
+        an, re, std = np.array(l1).flatten(), np.concatenate(l2).flatten(), np.concatenate(l3).flatten()
+        error.append(np.abs(an - re).mean())
+        error.append(np.abs((an - re) / an).mean() * 100)
+        error.append(np.mean(std))
+
+    with open(eval_file, "w") as file:
+        wr = csv.writer(file)
+        wr.writerow(header)
+        wr.writerow(error)
+
+
+if __name__ == "__main__":
+    parser = build_parser()
+    args = parser.parse_args()
+    main(args)
