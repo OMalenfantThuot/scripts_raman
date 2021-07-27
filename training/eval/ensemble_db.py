dba97fff9ecd0183fca403a39b99b35062ce37b6
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2021-07-27 16:11:49 -0400

    ensemble evaluation with full details

--- training/eval/ensemble_db.py
+++ training/eval/ensemble_db.py
@@ -8,18 +8,24 @@ import numpy as np
 import mlcalcdriver
 from ase.db import connect
 from mlcalcdriver import Posinp, Job
+import pickle
 
 
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("dbpath", help="Path to the database to evaluate.")
     parser.add_argument("properties", help="Properties to evaluate.", nargs="*")
-    parser.add_argument("--models", help="Paths to the trained models.", default=[], nargs="*")
+    parser.add_argument(
+        "--models", help="Paths to the trained models.", default=[], nargs="*"
+    )
     parser.add_argument("--name", help="Name of the evaluation file.", default=None)
     parser.add_argument(
         "--overwrite", help="Remove previous evaluation file", action="store_true"
     )
     parser.add_argument("--cuda", help="Use GPU for evaluation", action="store_true")
+    parser.add_argument(
+        "--detailed", action="store_true", help="Get the values for each data point."
+    )
     return parser
 
 
@@ -36,9 +42,7 @@ def main(args):
             )
     device = "cuda" if args.cuda else "cpu"
 
-    calculator = mlcalcdriver.calculators.EnsembleCalculator(
-        args.models, device=device
-    )
+    calculator = mlcalcdriver.calculators.EnsembleCalculator(args.models, device=device)
 
     with connect(args.dbpath) as db:
         answers, results, stds = (
@@ -58,18 +62,34 @@ def main(args):
                 stds[i].append(job.results[prop + "_std"].tolist())
 
     header, error = [], []
+    if args.detailed:
+        full_err, full_std = {}, {}
     for prop in args.properties:
         header += ["MAE_" + prop, "%Error_" + prop, "STD_" + prop]
-    for l1, l2, l3 in zip(answers, results, stds):
-        an, re, std = np.array(l1).flatten(), np.concatenate(l2).flatten(), np.concatenate(l3).flatten()
+        if args.detailed:
+            full_err[prop], full_std[prop] = [], []
+    for p, l1, l2, l3 in zip(args.properties, answers, results, stds):
+        an, re, std = (
+            np.array(l1).flatten(),
+            np.concatenate(l2).flatten(),
+            np.concatenate(l3).flatten(),
+        )
         error.append(np.abs(an - re).mean())
         error.append(np.abs((an - re) / an).mean() * 100)
         error.append(np.mean(std))
+        if args.detailed:
+            full_err[p].append(np.abs(an - re))
+            full_std[p].append(std)
 
     with open(eval_file, "w") as file:
         wr = csv.writer(file)
         wr.writerow(header)
         wr.writerow(error)
+    if args.detailed:
+        with open(eval_name + "_err.pkl", "wb") as f:
+            pickle.dump(full_err, f)
+        with open(eval_name + "_std.pkl", "wb") as f:
+            pickle.dump(full_std, f)
 
 
 if __name__ == "__main__":
