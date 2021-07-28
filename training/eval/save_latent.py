50ad0cf2fb28066752f7a86225e52e80619e21e6
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2021-07-28 15:04:25 -0400

    executable to save latent space positions from as database

--- training/eval/save_latent.py
+++ training/eval/save_latent.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 from ase.db import connect
 from schnetpack.utils import load_model
-from schnetpac
+from schnetpack.environment import AseEnvironmentProvider
+from mlcalcdriver.interfaces import SchnetPackData
+from schnetpack import AtomsLoader
 import argparse
 import torch
 import os
@@ -10,12 +14,29 @@ def main(args):
 
     device = "cuda" if args.cuda else "cpu"
     model = load_model(args.modelpath, map_location=device)
+    cutoff = float(model.representation.interactions[0].cutoff_network.cutoff)
     n_neurons = model.representation.n_atom_basis
+    individual_reps = []
 
     with connect(args.dbpath) as db:
         n_struct = db.count()
+        atoms = [row.toatoms() for row in db.select()]
+    data = SchnetPackData(
+        data=atoms,
+        environment_provider=AseEnvironmentProvider(cutoff=cutoff),
+        collect_triples=False,
+    )
+    data_loader = AtomsLoader(data, batch_size=1)
 
-    pass
+    for batch in data_loader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        rep = model.representation(batch)
+        individual_reps.append(rep)
+    representations = torch.cat(individual_reps).reshape(-1, n_neurons)
+
+    name = args.dbpath.split("/")[-1] + "_ls" if args.name is None else args.name
+    name = name + ".pt"
+    torch.save(representations, name)
 
 
 def create_parser():
