44a5a43b2b1a676a22e7fa2f6e7ae35203d421cf
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2022-03-16 16:46:09 -0400

    memory estimation for schnet models

--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,3 +1,7 @@
 from .model import *
 from .dropout_schnet import DropoutSchNet, DropoutAtomwise
 from .smoothtrainer import SmoothTrainer
+from .memory_estimation import (
+    schnet_memory_estimation_for_graphene,
+    get_graphene_patches_grid,
+)
