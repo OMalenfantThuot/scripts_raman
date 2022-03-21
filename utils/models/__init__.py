39692b121e6f593c42dcaa4c7a64d1789c4299ad
Olivier Malenfant-Thuot <malenfantthuotolivier@gmail.com>
2022-03-21 13:57:06 -0400

    temp commit

--- utils/models/__init__.py
+++ utils/models/__init__.py
@@ -1,3 +1,4 @@
 from .model import *
 from .dropout_schnet import DropoutSchNet, DropoutAtomwise
+from .patches_schnet import PatchesAtomisticModel, PatchesAtomwise
 from .smoothtrainer import SmoothTrainer
