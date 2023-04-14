fa9859ab8f6ac074d05df141d4ac374efef16020
OMalenfantThuot <malenfantthuotolivier@gmail.com>
2023-04-14 14:05:41 -0400

    untested

--- databases/dbcreator.py
+++ databases/dbcreator.py
@@ -18,64 +18,80 @@ def create_parser():
     parser.add_argument("dbname", help="Name of the database to create.")
     parser.add_argument(
         "run_mode",
-        choices=["abinit", "bigdft", "md"],
+        choices=["abinit", "bigdft", "md", "radnet"],
         help="Run mode used when generating the data.",
     )
+    parser.add_argument("--h5group", default=None, help="Name of the h5 group. Only for radnet.")
     return parser
 
 
 def main(args):
-    with connect(args.dbname) as db:
-        if args.run_mode == "bigdft":
-            db.metadata = DEFAULT_METADATA
-            files = [f for f in os.listdir() if f.endswith(".xyz")]
-            for f in files:
-                atoms = posinp_to_ase_atoms(Posinp.from_file(f))
-                with open(f, "r") as posinp_file:
-                    energy = float(posinp_file.readline().split()[2]) * 27.21138602
-                    forces = None
-                    for line in posinp_file:
-                        if "forces" not in line:
-                            continue
-                        else:
-                            forces = []
-                            for _ in range(len(atoms)):
-                                forces.append(
-                                    [
-                                        float(force)
-                                        for force in posinp_file.readline().split()[-3:]
-                                    ]
-                                )
-                            forces = np.array(forces) * 27.21138602 / 0.529177249
-                            break
-                db.write(atoms, data={"energy": energy, "forces": forces})
+    if args.run_mode in ["bigdft", "abinit", "md"]:
+        with connect(args.dbname) as db:
+            if args.run_mode == "bigdft":
+                db.metadata = DEFAULT_METADATA
+                files = [f for f in os.listdir() if f.endswith(".xyz")]
+                for f in files:
+                    atoms = posinp_to_ase_atoms(Posinp.from_file(f))
+                    with open(f, "r") as posinp_file:
+                        energy = float(posinp_file.readline().split()[2]) * 27.21138602
+                        forces = None
+                        for line in posinp_file:
+                            if "forces" not in line:
+                                continue
+                            else:
+                                forces = []
+                                for _ in range(len(atoms)):
+                                    forces.append(
+                                        [
+                                            float(force)
+                                            for force in posinp_file.readline().split()[-3:]
+                                        ]
+                                    )
+                                forces = np.array(forces) * 27.21138602 / 0.529177249
+                                break
+                    db.write(atoms, data={"energy": energy, "forces": forces})
 
-        elif args.run_mode == "abinit":
-            db.metadata = DEFAULT_METADATA
+            elif args.run_mode == "abinit":
+                db.metadata = DEFAULT_METADATA
+                files = [
+                    f for f in os.listdir() if f.endswith(".out") or f.endswith(".abo")
+                ]
+                for f in files:
+                    atoms = read(f, format="abinit-out")
+                    about = AbinitOutputFile(f)
+                    energy = float(about.final_vars_global["etotal"]) * 27.21138602
+                    forces = (
+                        np.array(
+                            [float(g) for g in about.final_vars_global["fcart"].split()]
+                        ).reshape(-1, 3)
+                        * 27.21138602
+                        / 0.529177249
+                    )
+                    db.write(atoms, data={"energy": energy, "forces": forces})
+
+            elif args.run_mode == "md":
+                db.metadata = DEFAULT_MD_METADATA
+                files = [f for f in os.listdir() if f.endswith(".xyz")]
+                for i, f in enumerate(files):
+                    atoms = read(f, format="extxyz")
+                    energy = atoms.get_total_energy()
+                    forces = atoms.get_forces()
+                    db.write(atoms, data={"energy": energy, "forces": forces})
+
+    elif args.run_mode in ["radnet"]:
+        h5FileName = args.dbname if args.dbname.endswith(".h5") else args.dbname + ".h5"
+        with h5py.File(h5FileName, "w") as f:
             files = [
                 f for f in os.listdir() if f.endswith(".out") or f.endswith(".abo")
             ]
             for f in files:
                 atoms = read(f, format="abinit-out")
                 about = AbinitOutputFile(f)
-                energy = float(about.final_vars_global["etotal"]) * 27.21138602
-                forces = (
-                    np.array(
-                        [float(g) for g in about.final_vars_global["fcart"].split()]
-                    ).reshape(-1, 3)
-                    * 27.21138602
-                    / 0.529177249
-                )
-                db.write(atoms, data={"energy": energy, "forces": forces})
 
-        elif args.run_mode == "md":
-            db.metadata = DEFAULT_MD_METADATA
-            files = [f for f in os.listdir() if f.endswith(".xyz")]
-            for i, f in enumerate(files):
-                atoms = read(f, format="extxyz")
-                energy = atoms.get_total_energy()
-                forces = atoms.get_forces()
-                db.write(atoms, data={"energy": energy, "forces": forces})
+
+
+
 
 
 if __name__ == "__main__":
